472a5ceacfc9cda72545537c2b563646ac2c02cd
Beomyeon <beomyeon.kim@adriel.ai>
2023-12-02 17:46:04 +0900

    chore: delete unnecessary check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -15,16 +15,6 @@ export class HealthController {
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([
-      () =>
-        this.http.pingCheck(
-          'Hanghae99',
-          'https://hanghae99.spartacodingclub.kr/',
-        ),
-      () =>
-        this.http.pingCheck('Adriel business page', 'https://www.adriel.com/'),
-      () => this.http.pingCheck('Adriel BI', 'https://app.adriel.com'),
-      () => this.http.pingCheck('Adgen AI', 'https://app.adgenai.com'),
-    ]);
+    return this.health.check([]);
   }
 }
