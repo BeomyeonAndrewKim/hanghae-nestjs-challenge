61d7d4ce01893b82cda059cf74b99d3310006524
Beomyeon <beomyeon.kim@adriel.ai>
2023-12-02 17:09:08 +0900

    feat: add adriel service health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -18,9 +18,13 @@ export class HealthController {
     return this.health.check([
       () =>
         this.http.pingCheck(
-          'hanghae99',
+          'Hanghae99',
           'https://hanghae99.spartacodingclub.kr/',
         ),
+      () =>
+        this.http.pingCheck('Adriel business page', 'https://www.adriel.com/'),
+      () => this.http.pingCheck('Adriel BI', 'https://app.adriel.com'),
+      () => this.http.pingCheck('Adgen AI', 'https://app.adgenai.com'),
     ]);
   }
 }
