de352feeabe0e3cbad5cd6ef17ab31d2576c77d8
Beomyeon <beomyeon.kim@adriel.ai>
2023-12-04 08:38:25 +0900

    feat: add env setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,12 @@ import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: false,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    HealthModule,
+  ],
 })
 export class AppModule {}
