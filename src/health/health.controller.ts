35524ed16f9471d0f11b293146e05fdc21af5fa7
Beomyeon <beomyeon.kim@adriel.ai>
2023-12-04 13:42:26 +0900

    feat: add db ping check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,20 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheckService,
-  HttpHealthIndicator,
   HealthCheck,
+  TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
-    private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([]);
+  }
+
+  @Get('/db')
+  @HealthCheck()
+  dbcheck() {
+    return this.db.pingCheck('database');
   }
 }
