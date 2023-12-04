1517481cff5501e14cce984418cbe2662c347b06
Beomyeon <beomyeon.kim@adriel.ai>
2023-12-04 23:28:04 +0900

    feat: set ssl for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { HealthModule } from './health/health.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+const EXTERNAL_DB_ENVS = new Set(['production', 'development']);
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,16 +13,28 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: parseInt(configService.get<string>('DB_PORT')),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '../**/*.entity{.ts,.js}'],
-        synchronize: configService.get<string>('DB_SYNCHRONIZE') === 'true',
-      }),
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+        const config: TypeOrmModuleOptions = {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: parseInt(configService.get<string>('DB_PORT')),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [__dirname + '../**/*.entity{.ts,.js}'],
+          synchronize: configService.get<string>('DB_SYNCHRONIZE') === 'true',
+          ...(EXTERNAL_DB_ENVS.has(process.env.NODE_ENV) && {
+            ssl: true,
+            extra: {
+              ssl: {
+                rejectUnauthorized: false,
+              },
+            },
+          }),
+        };
+
+        return config;
+      },
       inject: [ConfigService],
     }),
     HealthModule,
