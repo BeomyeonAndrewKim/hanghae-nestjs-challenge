ef6c9029b1564a51ad5f86637020b1ab27d92ede
Beomyeon <beomyeon.kim@adriel.ai>
2023-12-04 13:12:40 +0900

    feat: add typeorm config with env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: false,
       envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '../**/*.entity{.ts,.js}'],
+        synchronize: configService.get<string>('DB_SYNCHRONIZE') === 'true',
+      }),
+      inject: [ConfigService],
     }),
     HealthModule,
   ],
