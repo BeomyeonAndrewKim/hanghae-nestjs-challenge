8e545295e411cde52f3d7d469315c8bb48e7b98d
Andrea Kim <beomyeon.kim@adriel.ai>
2023-12-08 01:46:56 +0900

    fix: delete wrong provier module

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import {
-  HealthCheckService,
-  TerminusModule,
-  TypeOrmHealthIndicator,
-} from '@nestjs/terminus';
+import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [TerminusModule, HttpModule],
   controllers: [HealthController],
-  providers: [HealthCheckService, TypeOrmHealthIndicator],
 })
 export class HealthModule {}
