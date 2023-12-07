c6148337616d600a3f1212ef92a650a838e8bcc9
Andrea Kim <beomyeon.kim@adriel.ai>
2023-12-08 01:35:43 +0900

    fix: add service provider at health controller

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-import { TerminusModule } from '@nestjs/terminus';
+import {
+  HealthCheckService,
+  TerminusModule,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 import { HealthController } from './health.controller';
-import { HealthService } from './health.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [TerminusModule, HttpModule],
   controllers: [HealthController],
-  providers: [HealthService],
+  providers: [HealthCheckService, TypeOrmHealthIndicator],
 })
 export class HealthModule {}
